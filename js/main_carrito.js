6a7ba26e58abc339e19c32a7e1207ec74afd1daa
lisandro bustos <bustoslj16@gmail.com>
2024-08-08 16:24:46 -0300

    V5: Finalización del proyecto y pequeñas mejoras en el método de pago.

--- js/main_carrito.js
+++ js/main_carrito.js
@@ -3,19 +3,23 @@ function renderCarrito(){
     let contenidoHTML = `<table class="table">
     <tbody>
     <tr>
-    <td class="text-end" colspan="4"><button class="btn btn-danger brn-sm" onclick="vaciarCarrito();">Vaciar carrito <i class="bi bi-trash3-fill"></i></button></td>
+    <td class="text-end" colspan="4"><button class="btn btn-danger btn-sm" onclick="vaciarCarrito();">Vaciar carrito <i class="bi bi-trash3-fill"></i></button></td>
     </tr>`;
+    let hayProductos = false;
+    
     if (totalProductos() > 0) {
+        hayProductos = true;
         for (const producto of carrito){
             contenidoHTML += `<tr>
             <td><img src="../assets/imagenes-productos/${producto.imagen}" alt="${producto.nombre}" width="48"></td>
             <td class="align-middle">${producto.nombre}</td>
             <td class="text-center align-middle"><span class="card-text text-center text-danger">$${producto.precio} ARS</span></td>
-            <td class="text-end align-middle"><button class="btn btn-danger btn-sm" onclick="eliminarCarrito(${producto.id});";">Eliminar <i class="bi bi-x-circle"></i></button></td>
-            </tr>`
+            <td class="text-end align-middle"><button class="btn btn-danger btn-sm" onclick="eliminarCarrito(${producto.id});">Eliminar <i class="bi bi-x-circle"></i></button></td>
+            </tr>`;
         }
-                contenidoHTML +=`</tbody>
-        </table>`
+        
+        contenidoHTML +=`</tbody>
+        </table>`;
 
         contenidoHTML += `<table class="table">
         <tbody>
@@ -23,14 +27,13 @@ function renderCarrito(){
                 <td class="text-start align-middle"><input type="text" class="form-control" id="cupon" placeholder="Ingrese Su cupón Aquí"></td>
                 <td class="text-start align-middle"><button type="button" class="btn btn-primary" name="Enviar" value="Enviar" onclick="recibirCupon();">Confirmar Cupón</button></td>
                 <td class="text-danger text-end align-middle d-inline-flex p-3">
-                    <p id="mensajeFinal">El total de al compra es de: <strong>$</p>
+                    <p id="mensajeFinal">El total de la compra es de: <strong>$</p>
                     <p id="precioFinal"> ${calcularTotal()}</p></strong>
                 </td>
-            </tr>`
+            </tr>`;
 
         contenidoHTML +=`</tbody>
         </table>`;
-        obtenerPago();
     } else {
         contenidoHTML = `<div class="alert alert-dark my-5 text-center" role="alert">
         <h2>Lo sentimos mucho</h2>
@@ -40,8 +43,18 @@ function renderCarrito(){
 
     document.getElementById("contenidoCarrito").innerHTML = contenidoHTML;
 
-
+    return hayProductos;
 }
 
+// Función que ejecuta el método de pago si el carrito tiene productos
+function actualizarCarritoYPago() {
+    const hayProductos = renderCarrito();
+    renderBotonCarrito();
+    if (hayProductos) {
+        obtenerPago();
+    }
+}
+
+actualizarCarritoYPago();
 renderCarrito();
 renderBotonCarrito();
